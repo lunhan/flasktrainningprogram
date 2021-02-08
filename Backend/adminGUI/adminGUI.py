43cb436ff85bc4f7a7282f513a8acc693eb667c6
mhan823 <mlunhan97@gmail.com>
2021-02-08 22:59:05 +1300

    add todo v0

--- Backend/adminGUI/adminGUI.py
+++ Backend/adminGUI/adminGUI.py
@@ -1,16 +1,19 @@
 from flask import *
 
+# TODO:创建一个蓝图
 admin = Blueprint('admin', __name__)
 
+# TODO:把partner page作为打开网页的default页面
 @admin.route('/')
 def partner():
     return render_template('partner.html')
 
+# TODO:可以切换到members页面
 @admin.route('/members')
 def members():
     return render_template('members.html')
 
+# TODO:可以切换到event页面
 @admin.route('/event')
 def event():
     return render_template('event.html')
-
--- Backend/app.py
+++ Backend/app.py
@@ -1,8 +1,10 @@
 from flask import *
+#TODO:
 from Backend.adminGUI.adminGUI import admin
 
 app = Flask(__name__)
 
+#TODO:
 app.register_blueprint(admin)
 
 if __name__ == '__main__':
