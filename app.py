0ea57294b0db696c7946ecfc546da5a02acdbbdd
mhan823 <mlunhan97@gmail.com>
2021-02-09 01:03:10 +1300

    final design1

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 from flask import *
-#TODO:
-from Backend.adminGUI.adminGUI import admin
+from page.page import page
 
 app = Flask(__name__)
 
 #TODO:
-app.register_blueprint(admin)
+app.register_blueprint(page)
 
 if __name__ == '__main__':
     app.run()
--- page/page.py
+++ page/page.py
@@ -1,19 +1,19 @@
 from flask import *
 
 # TODO:创建一个蓝图
-admin = Blueprint('admin', __name__)
+page = Blueprint('page', __name__)
 
 # TODO:把partner page作为打开网页的default页面
-@admin.route('/')
+@page.route('/')
 def partner():
     return render_template('partner.html')
 
 # TODO:可以切换到members页面
-@admin.route('/members')
+@page.route('/members')
 def members():
     return render_template('members.html')
 
 # TODO:可以切换到event页面
-@admin.route('/event')
+@page.route('/event')
 def event():
     return render_template('event.html')
